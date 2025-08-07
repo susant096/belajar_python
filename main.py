4fa8dc87d8c1b12467bc7401558e481d82e4f0d8
susant096 <sazhunt@gmail.com>
2025-08-07 16:26:43 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+## Import library code
+from p5 import *
+from random import randint
+
+# The mouse_pressed function goes here
+def mouse_pressed():
+   # print('🎯')
+    if hit_colour == Color('blue').hex:
+        print('You hit the outer circle, 50 points!')
+    elif hit_colour == Color('red').hex:
+        print('You hit the inner circle, 200 points!')
+    elif hit_colour == Color('yellow').hex:
+        print('You hit the middle, 500 points!')
+    else:
+        print('you missed! no point')
+# The shoot_arrow function goes here
+def shoot_arrow():
+    global hit_colour
+    arrow_x=randint(100,300)
+    arrow_y=randint(100,300)
+    hit_colour = get(arrow_x,arrow_y).hex
+   # print(hit_colour)
+    fill("brown")
+    circle(arrow_x,arrow_y,15)
+
+def setup():
+    # Set up your game here
+    size(400, 400)
+    no_stroke()
+
+
+def draw():
+    # Things to do in every frame
+    fill("cyan")
+    rect(0, 0, 400, 250)
+    
+    fill("lightgreen")
+    rect(0,250,400,150)
+    
+    fill("brown")
+    triangle(150,350,200,150,250,350)
+    
+    fill("blue")
+    circle(200,200,170)
+    
+    fill("red")
+    circle(200,200,110)
+    
+    fill("yellow")
+    circle(200,200,30)
+    shoot_arrow()
+
+
+# Keep this to run your code
+run(frame_rate=2)
+
